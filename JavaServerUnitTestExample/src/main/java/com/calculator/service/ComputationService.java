8914330eba53172c93db40f2013703e6ba0bbe20
Chintan Dwivedi <chintandwivedi@AHMcdwivediDKG8WP.local>
2016-03-01 14:48:19 +0530

    added new computation service

---
+++ JavaServerUnitTestExample/src/main/java/com/calculator/service/ComputationService.java
@@ -0,0 +1,11 @@
+package com.calculator.service;
+
+public interface ComputationService {
+
+	String basicAirthmeticOperation(String experssion);
+	
+	String convertDecimalToBinary(String experssion);
+	
+	String convertUnitAndMeasure(String experssion);
+	
+}
---
+++ JavaServerUnitTestExample/src/main/java/com/calculator/service/ComputationServiceImpl.java
@@ -0,0 +1,20 @@
+package com.calculator.service;
+
+public class ComputationServiceImpl implements ComputationService {
+
+	@Override
+	public String basicAirthmeticOperation(String experssion) {
+		return null;
+	}
+
+	@Override
+	public String convertDecimalToBinary(String experssion) {
+		return null;
+	}
+
+	@Override
+	public String convertUnitAndMeasure(String experssion) {
+		return null;
+	}
+
+}
