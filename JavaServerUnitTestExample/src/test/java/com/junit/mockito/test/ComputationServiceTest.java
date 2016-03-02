70c71b44ea294a3ebd3db8e3a1b631be285bfef4
Chintan Dwivedi <chintandwivedi@AHMcdwivediDKG8WP.local>
2016-03-02 13:25:00 +0530

    refactor the code

--- JavaServerUnitTestExample/src/test/java/com/junit/mockito/test/ComputationServiceTest.java
+++ JavaServerUnitTestExample/src/test/java/com/junit/mockito/test/ComputationServiceTest.java
@@ -4,9 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.util.concurrent.atomic.AtomicLong;
-
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -21,7 +18,6 @@ public class ComputationServiceTest {
 
     private static ComputationService computationService;
 
-    private static AtomicLong executed = new AtomicLong(0);
 
     @BeforeClass
     public static void setupMocks() {
@@ -33,41 +29,33 @@ public class ComputationServiceTest {
 
     @Test
     public void testBasicAirthmeticOperation() {
-        executed.getAndIncrement();
+        when(computationEngine.computeOperation("2*2")).thenReturn("4");
         assertEquals(computationService.basicAirthmeticOperation("2*2"), "4");
         assertEquals(computationService.basicAirthmeticOperation("2+22"), "24");
     }
 
     @Test
     public void testConvertDecimalToBinary() {
-        executed.getAndIncrement();
         assertEquals(computationService.convertDecimalToBinary("219 to binary"), "11011011_2");
         assertEquals(computationService.convertDecimalToBinary("4242 to binary"), "1000010010010_2");
     }
 
     @Test
     public void testConvertUnitAndMeasure() {
-        executed.getAndIncrement();
         assertEquals(computationService.convertUnitAndMeasure("120 kilometers to meters"), "120000 meters");
         assertEquals(computationService.convertUnitAndMeasure("160 kilometers to miles"), "99.42 miles");
     }
 
     @Test(expected = ResultNotFoundException.class)
     public void testResultNotFoundException() {
-        executed.getAndIncrement();
         computationService.basicAirthmeticOperation("!@#$%^&*()!@#$%^&*()!@#$%^&*(");
     }
 
     @Test
     public void testIncompatibleConversion() {
-        executed.getAndIncrement();
         assertEquals(computationService.convertUnitAndMeasure("convert 1024 megabytes to miles"), " MB  (megabytes) and  miles are not compatible.");
     }
 
-    @AfterClass
-    public static void printDetails() {
-        System.out.println("Executed " + executed.longValue() + " test cases.");
-    }
 
     private static void setupMockMethods() {
         when(computationEngine.computeOperation("2*2")).thenReturn("4");
@@ -80,6 +68,7 @@ public class ComputationServiceTest {
         when(computationEngine.computeOperation("160 kilometers to miles")).thenReturn("99.42 miles");
 
         when(computationEngine.computeOperation("!@#$%^&*()!@#$%^&*()!@#$%^&*(")).thenThrow(new ResultNotFoundException("No result found for given query"));
-        when(computationEngine.computeOperation("convert 1024 megabytes to miles")).thenReturn(" MB  (megabytes) and  miles are not compatible.");
-    }
+		when(computationEngine.computeOperation("convert 1024 megabytes to miles"))
+				.thenReturn(" MB  (megabytes) and  miles are not compatible.");
+	}
 }
